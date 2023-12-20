f7ad48079e751ab93d74f8ebc924091a62bfe1fc
akshaya <akshayakrishnan318@gmail.com>
2023-12-20 10:40:05 +0530

    fetched table data

--- sihbackend/settings.py
+++ sihbackend/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = 'sihbackend.wsgi.application'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # Adjust this to match your React app's origin
     # all origins
+    'http://localhost:3001/',
     
 ]
 
